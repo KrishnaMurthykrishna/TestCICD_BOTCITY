cd5a0c35bd035bb7fd368fae25c3f4b0f8e50369
Krishna Murthy S <87650027+KrishnaMurthykrishna@users.noreply.github.com>
2026-04-17 17:05:37 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
-print("GIT CICD started")
+# Import for integration with BotCity Maestro SDK
+from botcity.maestro import (  # noqa: E402
+    BotMaestroSDK,
+    AlertType,
+    AutomationTaskFinishStatus,
+)
+
+# Disable errors if we are not connected to Maestro
+BotMaestroSDK.RAISE_NOT_CONNECTED = False
+
+
+def main():
+    # Runner passes the server url, the id of the task being executed,
+    # the access token and the parameters that this task receives (when applicable).
+    maestro = BotMaestroSDK.from_sys_args()
+    # Fetch the BotExecution with details from the task, including parameters
+    execution = maestro.get_execution()
+
+    print(f"Task ID is: {execution.task_id}")  # noqa: T201
+    print(f"Task Parameters are: {execution.parameters}")  # noqa: T201
+    print("starting...")
+    maestro.alert(task_id=execution.task_id, title="Example Bot started!", message="The bot has started its execution.", alert_type=AlertType.INFO)
+    maestro.finish_task(
+        task_id=execution.task_id,
+        status=AutomationTaskFinishStatus.SUCCESS,
+        message="Congratulations on finishing first task with success. 🎉",
+        
+    )
+
+if __name__ == "__main__":
+    main()
